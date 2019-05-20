95f641b1d19ae21777c18706814cdcf1423ffde2
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-20 17:21:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b(un{1}\w*ing\b)/).flatten()
+  text.scan(/\bun{1}\w*ing\b/)
+  # binding.pry
 end
 
 def words_five_letters_long(text)
